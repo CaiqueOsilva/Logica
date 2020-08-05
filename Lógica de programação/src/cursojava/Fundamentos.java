7ca504e1dedd0296a0b8563ad7aed7bce7fbcfd7
CaiqueOsilva <57421068+CaiqueOsilva@users.noreply.github.com>
2020-08-04 22:12:27 -0300

    Add files via upload

---
+++ Lógica de programação/src/cursojava/Fundamentos.java
@@ -0,0 +1,20 @@
+package cursojava;
+
+public class Fundamentos {
+
+	public static void main(String[] args) {
+		String nome; 
+		int idade = 27;
+		char sexo = 'm';
+		double temperatura = 26.7;
+		boolean arcondicionado = false;
+	    nome = "Caique Oliveira";
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+		System.out.println("Sexo: " + sexo);
+		System.out.println("Temperatura: " + temperatura);
+		System.out.println("Ar condicionado: " + arcondicionado);
+
+	}
+
+}
---
+++ Lógica de programação/src/cursojava/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package cursojava;
+
+/**
+ * @author Caique Oliveira 
+ * Hello World
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
